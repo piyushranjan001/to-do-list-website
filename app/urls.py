a308e402a9776016b0cc59cd2688c0fde0c99a2c
piyush <iamchouhan001@gmail.com>
2020-11-09 10:36:15 +0530

    initial commit

--- app/urls.py
+++ app/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
 from app import views
+
 # or from . import views
 urlpatterns = [
     path('',views.home,name='home'),
+    path('addTodo/',views.addTodo,name='addTodo'),
+    path('deleteTodo/<int:todo_id>/',views.deleteTodo,name='deleteTodo')
 ]
--- app/views.py
+++ app/views.py
@@ -1,7 +1,24 @@
 from django.shortcuts import render
-from django.shortcuts import HttpResponse
+from django.shortcuts import HttpResponseRedirect,HttpResponse
+from .models import TodoItem
 
 # Create your views here.
 def home(request):
     # return HttpResponse("This is todolist")
-    return render(request,'basic.html')
+    all_to_do_items = TodoItem.objects.all()
+    return render(request,'basic.html',{'all_items':all_to_do_items})
+
+def addTodo(request):
+    # create a new to do item
+    # save it
+    # rediret to browser /todo/
+    # c = request.POST['content']
+    # new_item = TodoItem(content=c)
+    new_item = TodoItem(content= request.POST["content"])
+    new_item.save()
+    return HttpResponseRedirect('/app/')
+
+def deleteTodo(request,todo_id):
+    item_to_delete = TodoItem.objects.get(id=todo_id)
+    item_to_delete.delete()
+    return HttpResponseRedirect('/app/')
